623e1411d5aff2dfdd507713112e5fa798d76cbe
Chelase <2911996843@qq.com>
2023-12-06 09:44:39 +0800

    插件错误

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-  <div>我是Home页</div>
+  <div>
+    <p>我是Home页</p>
+    <p class="test">test</p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.test{
+  color: $priceColor;
+}
 </style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,16 @@
 <script setup>
-
+  import Nav from '@/views/Layout/components/Nav.vue'
+  import Header from '@/views/Layout/components/Header.vue'
+  import Footer from '@/views/Layout/components/Footer.vue'
 </script>
 
 <template>
   <div>我是首页</div>
   <!--二级路由出口-->
+  <Nav/>
+  <Header/>
   <router-view/>
+  <Footer/>
 </template>
 
 <style scoped>
